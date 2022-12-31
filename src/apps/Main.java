52696a2cd4f813d4b7f12f0986e865352acd3014
Hadad Al Akbar <akbarhadadal@gmail.com>
2022-12-31 13:40:54 +0700

    update changing button click

--- src/apps/Main.java
+++ src/apps/Main.java
@@ -1,43 +1,42 @@
 package apps;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-    //define your offsets here
+    // define your offsets here
     private double xOffset = 0;
     private double yOffset = 0;
-    
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Sidebar.fxml"));
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        
-        //grab your root here
+
+        // grab your root here
         root.setOnMousePressed(event -> {
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
         });
 
-        //move around here
+        // move around here
         root.setOnMouseDragged(event -> {
             primaryStage.setX(event.getScreenX() - xOffset);
             primaryStage.setY(event.getScreenY() - yOffset);
         });
         Scene scene = new Scene(root);
-        //set transparent
+        // set transparent
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
-        primaryStage.show();
-    }
 
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/apps/Sidebar.java
@@ -0,0 +1,66 @@
+package apps;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Sidebar implements Initializable {
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Button btnBooks;
+
+    @FXML
+    private Pane pnlDashboard;
+
+    @FXML
+    private AnchorPane pnlChange;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("DashboardPage.fxml"));
+            Parent root = loader.load();
+
+            pnlChange.getChildren().setAll(root);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        if (event.getSource() == btnBooks) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("BooksPage.fxml"));
+                Parent root = loader.load();
+
+                pnlChange.getChildren().setAll(root);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+
+        } else if(event.getSource() == btnDashboard) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("DashboardPage.fxml"));
+                Parent root = loader.load();
+
+                pnlChange.getChildren().setAll(root);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+}
+
+}
